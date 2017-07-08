c59899989bb3575742c7a7b0d241208af79aa228
LeeSef <liyuhui644504@qq.com>
2017-07-09 00:15:15 +0800

    Hello MyEclipse 2017.07.09

---
+++ src/HelloMyEclipse.java
@@ -0,0 +1,12 @@
+public class HelloMyEclipse {
+
+	public static void main(String[] arg) {
+
+		// 注释，选中需要注释的行 ctrl+/ 或ctrl+shift+/
+		// 控制台输出：Hello MyEclipse !!!
+		System.out.println("Hello MyEclipse !!!");
+		// 格式化代码ctrl+shift+f,代码就会自动排版
+		// 我们写实体类的时候需要生成get/set方法，按住art+shift不要松开先按S键，在按R键，在按A键
+	}
+
+}
